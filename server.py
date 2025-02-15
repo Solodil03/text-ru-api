532d950129769d3c59a5658595eda2120e796125
Solodil03 <151837101+Solodil03@users.noreply.github.com>
2025-02-15 21:01:53 +0700

    server.py

--- server.py
+++ server.py
@@ -59,6 +59,6 @@ def get_result(uid: str):  # Параметр теперь uid
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
 
 
